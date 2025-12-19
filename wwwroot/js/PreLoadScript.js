46eb90f5af789ca0bae55c109be7c657cd7e2afc
AhmedHasnain <ahmed.hasnain557@gmail.com>
2025-12-20 00:06:43 +0500

    Add files via upload

---
+++ wwwroot/js/PreLoadScript.js
@@ -0,0 +1,64 @@
+﻿
+
+
+function toastHandle(toastType, toastMessage) {
+
+    let waiting = setInterval(() => {
+
+        if (document.querySelector("#ToastSuccess")) {
+            let toastSucess = document.querySelector("#ToastSuccess")
+            let toastError = document.querySelector("#ToastError")
+
+            switch (toastType) {
+
+                case "error":
+                    document.querySelector("#ToastErrorMessage").innerText = toastMessage
+                    gsap.to(toastError, {
+                        display: "flex",
+                        right: "20px"
+                    })
+                    setTimeout(() => {
+                        gsap.to(toastError, {
+                            right: "-300px",
+                            display: "none"
+
+                        })
+                    }, 3000);
+                    break;
+
+                case "success":
+
+                    document.querySelector("#ToastSuccessMessage").innerText = toastMessage
+                    gsap.to(toastSucess, {
+                        display: "flex",
+                        right: "20px"
+                    })
+                    setTimeout(() => {
+                        gsap.to(toastSucess, {
+                            right: "-300px",
+                            display: "none"
+                        })
+                    }, 3000);
+                    break;
+            }
+            clearInterval(waiting)
+
+        }
+    }, 1000);
+
+}
+
+function ExpandNote(e) {
+    document.getElementById("ExpandNote").classList.replace("hidden", "flex")
+    let targetNote = e.currentTarget.closest(".note")
+    let TargetNoteHead = targetNote.querySelector(".NoteHead").innerText
+    let TargetNotePara = targetNote.querySelector(".NotePara").innerText
+    let ExpandNoteHead = document.querySelector("#ExpandNoteHeading")
+    let ExpandNotePara = document.querySelector("#ExpandNotePara")
+
+    ExpandNoteHead.innerText = TargetNoteHead
+    ExpandNotePara.innerText = TargetNotePara
+}
+function closeExpendNote() {
+    document.getElementById("ExpandNote").classList.replace("flex", "hidden")
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+let dropdownToggle = document.getElementById('dropdownToggle') || null;
+let dropdownMenu = document.getElementById('dropdownMenu') || null;
+
+function handleClick() {
+    if (dropdownMenu.className.includes('block')) {
+        dropdownMenu.classList.add('hidden')
+        dropdownMenu.classList.remove('block')
+    } else {
+        dropdownMenu.classList.add('block')
+        dropdownMenu.classList.remove('hidden')
+    }
+}
+
+if (dropdownToggle) dropdownToggle.addEventListener('click', handleClick);
+
+
+
